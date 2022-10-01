ce9c5b5b67403cfce9c9d984faa9418c1348c884
Kacper Sleziak <kacpersleziak00@gmail.com>
2022-10-01 10:11:24 +0200

    Configurated Silk Tool

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,4 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls), path("budget/", include("budget.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("budget/", include("budget.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [path("silk/", include("silk.urls", namespace="silk"))]
